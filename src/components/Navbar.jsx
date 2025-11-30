c05c3a65ababbbb5b658fa2204331c7a5186517b
Somnath Banerjee <aryansom80@gmail.com>
2025-11-30 14:01:34 +0530

    updated links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -133,8 +133,8 @@ const releasedSoftwares = Object.freeze([
   { name: "Stock Averaging", href: "https://openroot.in/stock-averaging/", icon: Icons.stockAveraging },
   { name: "SIP Return Analyzer", href: "https://openroot.in/return-analyzer/", icon: Icons.sipAnalyzer },
   { name: "Gold Jewel Price Indicator", href: "https://openroot.in/gold-jewel-price-indicator/", icon: Icons.goldJewel },
-  { name: "Credit Card EMI Calculator", href: "https://openroot.in/Credit-Card-EMI-Calculator/", icon: Icons.creditEmi },
-  { name: "Travel Expense Manager", href: "https://openroot.in/travel-expense-manager/", icon: Icons.travelExpense },
+  { name: "Credit Card EMI Calculator", href: "https://openroot.in/openroot-creditcard-emi-analyzer/", icon: Icons.creditEmi },
+  { name: "Travel Expense Manager", href: "https://openroot.in/openroot-travel-expense-manager/", icon: Icons.travelExpense },
   { name: "Makaut Grade Calculator", href: "https://openroot.in/makaut-grade-calculator/", icon: Icons.gradeCalculator },
   { name: "Helping Hand", href: "https://openroot.in/openroot-helping-hand/", icon: Icons.helpingHand },
 ]);
